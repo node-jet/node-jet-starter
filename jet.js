7adfeb520261776a5a57109b42e68fed59e7af0a
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2022-04-12 09:10:43 +0100

    update jet command

--- jet.js
+++ jet.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { JetCli, JetConsoleKernel } = require('node-jet-console');
+const { JetCli, JetConsoleKernel } = require('jetngin-console');
 const { AppCliKernel } = require('./src/console/kernel');
 
 JetCli(__dirname, [JetConsoleKernel, AppCliKernel])
